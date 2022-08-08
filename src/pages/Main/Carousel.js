748b2babac809887f65c13242b3d0a563fb70cf7
Koodanhee <k.danhee@gmail.com>
2022-08-08 17:36:52 +0900

    carousel 완료

--- src/pages/Main/Carousel.js
+++ src/pages/Main/Carousel.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Slider from 'react-slick';
+import './slick.css';
+import './slick-theme.css';
+import S from './Styled.Carousel';
 
 const Carousel = () => {
-  return <div />;
+  const [mainCarouselData, setMainCarouselData] = useState([]);
+  useEffect(() => {
+    fetch('/data/MainCarouselData.json')
+      .then(res => res.json())
+      .then(result => {
+        setMainCarouselData(result);
+      });
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1900,
+  };
+  return (
+    <S.Slick>
+      <Slider {...settings}>
+        <S.CarouselData alt="carousel" src={mainCarouselData[3]?.src} />
+        <S.CarouselData alt="carousel" src={mainCarouselData[0]?.src} />
+        <S.CarouselData alt="carousel" src={mainCarouselData[4]?.src} />
+        <S.CarouselData alt="carousel" src={mainCarouselData[2]?.src} />
+        <S.CarouselData alt="carousel" src={mainCarouselData[1]?.src} />
+        <S.CarouselData alt="carousel" src={mainCarouselData[5]?.src} />
+      </Slider>
+    </S.Slick>
+  );
 };
 
 export default Carousel;
---
+++ src/pages/Main/Styled.Carousel.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const S = {};
+
+S.CarouselData = styled.img`
+  width: 590px;
+  margin-right: 20px;
+  margin-top: 20px;
+  object-fit: cover;
+  cursor: pointer;
+`;
+
+S.Slick = styled.div`
+  justify-content: center;
+  align-items: center;
+  width: 1200px;
+  margin: 0 auto;
+`;
+
+export default S;
